3cf109de066f369805244735594079f9e4b8fb56
Joshua Beck <82618412+joshuazbeck@users.noreply.github.com>
2024-04-01 17:17:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
